4028fad34df9648e839a7b098231b22f8f15c4e9
Loc Pham <148402164+locpham126@users.noreply.github.com>
2024-01-07 23:06:14 -0500

    added annotations for bakerController

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -2,32 +2,38 @@ package com.zipcodewilmington.bakery.controllers;
 
 import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("bakers")
 public class BakerController {
     private BakerService service;
-
+    @Autowired
     public BakerController(BakerService service) {
         this.service = service;
     }
-
+    @RequestMapping(value ="/index")
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
-
+    @RequestMapping(value = "/{bakerId}", method = RequestMethod.GET)
     public ResponseEntity<Baker> show(Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
-
+    @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Baker> create(Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
-
+    @RequestMapping(value = "/update/{bakerId}")
     public ResponseEntity<Baker> update(Long id, Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
-
+    @RequestMapping(value ="/destroy/{bakerId}" )
     public ResponseEntity<Boolean> destroy(Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
